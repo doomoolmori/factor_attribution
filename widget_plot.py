558aa324bef454e8c03765da245808f8cac15573
Jamal <byungju.kim@doomoolmori.com>
2022-07-07 18:58:53 +0900

    box 플랏 추가

--- widget_plot.py
+++ widget_plot.py
@@ -29,6 +29,13 @@ using_factor = ['Bankruptcy Score_f_mean', 'Contrarian_f_mean', 'Dividend_f_mean
              'cpi_beta', 'cpi_beta.bull', 'cpi_beta.bear',
              'realrate_beta', 'realrate_beta.bull', 'realrate_beta.bear']
 
+box_plot_factor = ['Bankruptcy Score_f_mean', 'Contrarian_f_mean', 'Dividend_f_mean', 'Earning Momentum_f_mean',
+             'Earnings Quality_f_mean', 'Financial Quality_f_mean', 'Growth_f_mean', 'Information Uncertainty_f_mean',
+             'Investment_f_mean', 'Leverage_f_mean', 'Liquidity_f_mean', 'Momentum_f_mean', 'Size_f_mean',
+             'Value_f_mean']
+
+
+
 class MyWindow(QMainWindow):
     def __init__(self, calculation):
         super().__init__()
@@ -82,7 +89,11 @@ class MyWindow(QMainWindow):
         except:
             pass
         try:
-            self.box_and_scatter_widget.close()
+            self.box_widget.close()
+        except:
+            pass
+        try:
+            self.scatter_widget.close()
         except:
             pass
         try:
@@ -92,7 +103,6 @@ class MyWindow(QMainWindow):
 
     def setting_dialog_open(self):
         self.close_widget()
-
         ca_list = np.array([int(self.price_box.currentText().split(' : ')[-1]), \
                             int(self.dividend_box.currentText().split(' : ')[-1]), \
                             int(self.momentum_box.currentText().split(' : ')[-1]), \
@@ -108,11 +118,13 @@ class MyWindow(QMainWindow):
         calculation_result = self.calculation.make_result_dict(ca_list=ca_list, top_N=top_N)
 
         self.surface_and_bar_widget = QWidget()
-        self.box_and_scatter_widget = QWidget()
+        self.box_widget = QWidget()
+        self.scatter_widget = QWidget()
         self.table_widget = QWidget()
 
         self.weight_surface_and_bar(widget=self.surface_and_bar_widget, calculation_result=calculation_result)
-        self.score_box_and_scatter(widget=self.box_and_scatter_widget, calculation_result=calculation_result)
+        self.score_box_plot(widget=self.box_widget, calculation_result=calculation_result)
+        self.score_scatter_plot(widget=self.scatter_widget, calculation_result=calculation_result)
         self.final_table(widget=self.table_widget, calculation_result=calculation_result)
 
     ### Surface_and_bar
@@ -162,38 +174,80 @@ class MyWindow(QMainWindow):
         widget.weight_bar.legend()
 
     ### Box_and_scatter
-    def score_box_and_scatter(self, widget, calculation_result):
-        self.box_and_scatter_canvas(widget)
+    def score_box_plot(self, widget, calculation_result):
+        self.box_plot_canvas(widget)
         self.plot_score_box(widget, calculation_result)
-        self.plot_score_scatter(widget, calculation_result)
 
-    def box_and_scatter_canvas(self, widget):
-        widget.setWindowTitle("box_scatter")
+    def box_plot_canvas(self, widget):
+        widget.setWindowTitle("box_plot")
         widget.setGeometry(500, 100, 2000, 1000)
 
-        canvas_box = FigureCanvas(Figure(figsize=(4, 3)))
+        canvas_box0 = FigureCanvas(Figure(figsize=(4, 3)))
         vbox = QVBoxLayout(widget)
-        vbox.addWidget(canvas_box)
-        widget.box = canvas_box.figure.subplots()
+        vbox.addWidget(canvas_box0)
+        widget.box0 = canvas_box0.figure.subplots()
+
+        canvas_box1 = FigureCanvas(Figure(figsize=(4, 3)))
+        vbox.addWidget(canvas_box1)
+        widget.box1 = canvas_box1.figure.subplots()
+        widget.show()
+
+    def plot_score_box(self, widget, result):
+        df = result['normalized_factor_df']
+        final_df = result['final_factor_df']
+        widget.box0.boxplot(np.array(df))
+        for i, score_name in enumerate(self.calculation.char_list):
+            final_score = df[self.calculation.char_factor_mapping_dict[score_name]].loc[final_df.index]
+            y = np.array([final_score.mean() for x in range(5)])
+            x = np.array([i + 0.98, i + 0.99, i + 1, i + 1.01, i + 1.02])
+            widget.box0.plot(x, y, 'r.', alpha=1)
+        widget.box0.set_xticklabels(labels=self.calculation.char_list, fontsize=8)
+        title = ''
+        for i, name in enumerate(self.calculation.char_list):
+            title = f'{title}  {name}:{self.ca_list[i]}'
+        widget.box0.set_title(title)
+
+        df1 = self.calculation.original_factor_characters_df[box_plot_factor]
+        normalized_df1 = df1.apply(lambda x: self.calculation.calculation_normalized(x), axis=0)
+        widget.box1.boxplot(np.array(normalized_df1))
+        for i, score_name in enumerate(normalized_df1.columns):
+            final_score = normalized_df1[score_name].loc[final_df.index]
+            y = np.array([final_score.mean() for x in range(5)])
+            x = np.array([i + 0.98, i + 0.99, i + 1, i + 1.01, i + 1.02])
+            widget.box1.plot(x, y, 'r.', alpha=1)
+        title_list = []
+        for name in box_plot_factor:
+            title_list.append(name.split('_')[0])
+        widget.box1.set_xticklabels(labels=title_list, fontsize=8)
+
+
+    def score_scatter_plot(self, widget, calculation_result):
+        self.three_d_scatter_canvas(widget)
+        self.plot_score_scatter(widget, calculation_result)
+
+    def three_d_scatter_canvas(self, widget):
+        widget.setWindowTitle("3d scatter")
+        widget.setGeometry(500, 100, 2000, 1000)
 
         canvas_scatter = FigureCanvas(Figure(figsize=(4, 3)))
+        vbox = QVBoxLayout(widget)
         vbox.addWidget(canvas_scatter)
         widget.scatter = [canvas_scatter.figure.add_subplot(1, 2, 1, projection='3d'),
                           canvas_scatter.figure.add_subplot(1, 2, 2, projection='3d')]
 
         widget.x0_box = QComboBox(widget)
-        widget.x0_box.setGeometry(10, 500, 200, 30)
+        widget.x0_box.setGeometry(10, 10, 200, 30)
         widget.y0_box = QComboBox(widget)
-        widget.y0_box.setGeometry(10, 550, 200, 30)
+        widget.y0_box.setGeometry(10, 60, 200, 30)
         widget.z0_box = QComboBox(widget)
-        widget.z0_box.setGeometry(10, 600, 200, 30)
+        widget.z0_box.setGeometry(10, 110, 200, 30)
 
         widget.x1_box = QComboBox(widget)
-        widget.x1_box.setGeometry(1000, 500, 200, 30)
+        widget.x1_box.setGeometry(1000, 10, 200, 30)
         widget.y1_box = QComboBox(widget)
-        widget.y1_box.setGeometry(1000, 550, 200, 30)
+        widget.y1_box.setGeometry(1000, 60, 200, 30)
         widget.z1_box = QComboBox(widget)
-        widget.z1_box.setGeometry(1000, 600, 200, 30)
+        widget.z1_box.setGeometry(1000, 110, 200, 30)
 
         for factor_name in using_factor:
             widget.x0_box.addItem(f'x : {factor_name}')
@@ -210,28 +264,12 @@ class MyWindow(QMainWindow):
         widget.y1_box.setCurrentText(f'y : SD')
         widget.z1_box.setCurrentText(f'z : maxDrawdown')
 
-
-
         widget.scatter0_btn = QPushButton("scatter0", widget)
-        widget.scatter0_btn.move(310, 500)
+        widget.scatter0_btn.move(310, 10)
         widget.scatter1_btn = QPushButton("scatter1", widget)
-        widget.scatter1_btn.move(1310, 500)
+        widget.scatter1_btn.move(1310, 10)
         widget.show()
 
-    def plot_score_box(self, widget, result):
-        df = result['normalized_factor_df']
-        final_df = result['final_factor_df']
-        widget.box.boxplot(np.array(df))
-        for i, score_name in enumerate(self.calculation.char_list):
-            final_score = df[self.calculation.char_factor_mapping_dict[score_name]].loc[final_df.index]
-            y = np.array([final_score.mean() for x in range(5)])
-            x = np.array([i + 0.98, i + 0.99, i + 1, i + 1.01, i + 1.02])
-            widget.box.plot(x, y, 'r.', alpha=1)
-        widget.box.set_xticklabels(labels=self.calculation.char_list, fontsize=8)
-        title = ''
-        for i, name in enumerate(self.calculation.char_list):
-            title = f'{title}  {name}:{self.ca_list[i]}'
-        widget.box.set_title(title)
 
     def plot_score_scatter(self, widget, result):
         final_df = result['final_factor_df']
